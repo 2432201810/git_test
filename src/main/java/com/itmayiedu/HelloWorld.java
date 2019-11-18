3927c95118ba00a4772e30a0deb4c18557da8656
lige <2432204810@qq.com>
2019-11-18 20:09:06 +0800

    test

--- src/main/java/com/itmayiedu/HelloWorld.java
+++ src/main/java/com/itmayiedu/HelloWorld.java
@@ -3,5 +3,8 @@ package com.itmayiedu;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("lige");
+        String li="ge";
+        System.out.println(li);
     }
 }
