24474b80fa26a333be761db31676e79533872e8e
lige <2432204810@qq.com>
2019-11-19 10:17:41 +0800

    Merge branch 'master' of F:\Project\JavaEE\Git\git_test with conflicts.

--- src/main/java/com/itmayiedu/HelloWorld.java
+++ src/main/java/com/itmayiedu/HelloWorld.java
@@ -2,12 +2,10 @@ package com.itmayiedu;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        //abc
         System.out.println("HelloWorld123");
-        String s="xiao";
-        System.out.println(s);
         System.out.println("lige456");
         String li="ge789";
         System.out.println(li);
+        System.out.println("解决冲突问题");
     }
 }
