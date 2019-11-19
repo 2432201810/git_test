e9e1013e7aef3dd2131d877a1793270f5383cfb2
2432201810 <57379371+2432201810@users.noreply.github.com>
2019-11-19 10:03:54 +0800

    Update HelloWorld.java

--- src/main/java/com/itmayiedu/HelloWorld.java
+++ src/main/java/com/itmayiedu/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         //abc
         System.out.println("HelloWorld123");
+        String s="xiao";
+        System.out.println(s);
         System.out.println("lige456");
         String li="ge789";
         System.out.println(li);
