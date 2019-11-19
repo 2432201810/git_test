e20b0787ca1c1dff87a47c143d472d9d67371cd1
2432201810 <57379371+2432201810@users.noreply.github.com>
2019-11-19 09:56:05 +0800

    Update HelloWorld.java
    添加注释abc

--- src/main/java/com/itmayiedu/HelloWorld.java
+++ src/main/java/com/itmayiedu/HelloWorld.java
@@ -2,6 +2,7 @@ package com.itmayiedu;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        //abc
         System.out.println("HelloWorld123");
         System.out.println("lige456");
         String li="ge789";
